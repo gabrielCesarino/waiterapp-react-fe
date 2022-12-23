d1808815dbba146b87ba30ccdb7c468c10763af6
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-12-22 22:41:46 -0300

    add: Orders.ts type

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,5 +1,26 @@
 import { Container } from './styles';
 import { OrdersBoard } from '../OrdersBoard';
+import { Order } from '../../types/Order';
+
+const orders: Order[] = [
+	{
+		'table': '123',
+		'status': 'WAITING',
+		'products': [
+			{
+				'product': {
+					'name': 'Pizza',
+					'description': 'a',
+					'price': 2,
+					'imagePath': 'aaa'
+				},
+				'quantity': 2,
+				'_id': '63a35b3cc39a5135057b94e4'
+			}
+		],
+		'_id': '63a35b3cc39a5135057b94e3',
+	}
+];
 
 export function Orders() {
 	return(
@@ -7,14 +28,17 @@ export function Orders() {
 			<OrdersBoard
 				icon="🕑"
 				title= "Fila de espera"
+				orders={orders}
 			/>
 			<OrdersBoard
 				icon="👨‍🍳"
 				title= "Em preparação"
+				orders={[]}
 			/>
 			<OrdersBoard
 				icon="✔️"
 				title= "Pronto!"
+				orders={[]}
 			/>
 		</Container>
 	);
--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,8 +1,10 @@
+import { Order } from '../../types/Order';
 import { Board, OrdersContainer } from './styles';
 
 interface OrdersBoardProps {
 	icon: string,
 	title: string,
+	orders: Order[];
 }
 
 export function OrdersBoard(props: OrdersBoardProps) {
